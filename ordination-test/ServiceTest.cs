1171e6549630d013258b2255d80f170208b74546
Magni <jakobmagni@gmail.com>
2023-12-06 09:05:28 +0100

    Navngivning på testcases

--- ordination-test/ServiceTest.cs
+++ ordination-test/ServiceTest.cs
@@ -20,13 +20,13 @@ public class ServiceTest
         service = new DataService(context);
         service.SeedData();
     }
-    //TC1
+
     [TestMethod]
     public void PatientsExist()
     {
         Assert.IsNotNull(service.GetPatienter());
     }
-    //TC2
+    
     [TestMethod]
     public void OpretDagligFast()
     {
@@ -35,30 +35,30 @@ public class ServiceTest
         Patient patient = service.GetPatienter().First();
         Laegemiddel lm = service.GetLaegemidler().First();
 
+        
         Assert.AreEqual(1, service.GetDagligFaste().Count());
 
         service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId,
             2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
 
+        //TC1: Tjekker om der bliver oprettet en ny ordination
         Assert.AreEqual(2, service.GetDagligFaste().Count());
 
-        // Kast Exception hvis dato ligger før dags dato
-        Assert.ThrowsException<InvalidOperationException>(() =>
-            service.OpretDagligFast(2, lm.LaegemiddelId, 2, 2, 1, 0, dateInPast, DateTime.Now.AddDays(3)));
-
-        // Kast Exception hvis antal er negativ
+        //TC2: Kast Exception hvis antal er negativ
         Assert.ThrowsException<InvalidOperationException>(() =>
             service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId, 2, -4, 1, 0, DateTime.Now, DateTime.Now.AddDays(3)));
 
-        // Kast Exception hvis patient ikke eksisterer
+        //TC3: Kast Exception hvis patient ikke eksisterer
         Assert.ThrowsException<InvalidOperationException>(() =>
             service.OpretDagligFast(104, lm.LaegemiddelId, 2, -4, 1, 0, DateTime.Now, DateTime.Now.AddDays(3)));
 
-        
+        //TC4: Kast Exception hvis dato ligger før dags dato
+        Assert.ThrowsException<InvalidOperationException>(() =>
+            service.OpretDagligFast(2, lm.LaegemiddelId, 2, 2, 1, 0, dateInPast, DateTime.Now.AddDays(3)));
+
 
     }
 
-    //TC3
     [TestMethod]
     public void TestAtKodenSmiderEnException()
     {
